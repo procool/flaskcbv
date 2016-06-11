3e5412a4ff66693b1253ac5f32f39783c2f8262a
procool <ya.procool@ya.ru>
2016-06-11 23:16:40 +0300

    templatetags loader update

--- flaskcbv/templates/__init__.py
+++ flaskcbv/templates/__init__.py
@@ -18,12 +18,19 @@ def register_tag(environment, dirpath, name):
         if not extension.enabled:
             continue
         environment.add_extension(extension)
+        try: tags = extension.tags
+        except: tags = ''
+        
+        logging.info('FOUND Template Extention: %s: %s' % (name_, tags))
         ## print environment.extensions
 
 
 def register_tags(environment):
     dirpath = 'templatetags'
     path_ = get_templatetags(dirpath)
+    if path_ is None:
+        return
+    loaded = []
     try:
         for name in os.listdir(path_):
             fpath = os.path.abspath("/".join((path_, name)))
@@ -32,8 +39,16 @@ def register_tags(environment):
             if not stat.S_ISREG(fstat.st_mode):
                 continue
 
-            name = name[0:-3]
+            name, name_extension = os.path.splitext(name)
+            if name_extension not in ('.py', '.pyc'):
+                continue
+            if [path_, name] in loaded:
+                continue
             try: register_tag(environment, dirpath, name)
-            except: pass
+            except Exception as err:
+                logging.error('Error on register templatetags %s with path "%s": %s' % (name, path_, err))
+                continue
+            logging.debug('Register templatetags %s with path "%s": Done!' % (name, path_))
+            loaded.append([path_, name])
     except Exception as err:
         logging.error('Error on register templatetags with path "%s": %s' % (path_, err))
